1615742ab0f738bce8015eef408d1176402f76c8
om-dhawale <219327955+om-dhawale@users.noreply.github.com>
2026-04-22 19:54:04 +0530

    switch to DATABASE_URL for Neon

--- server/db.js
+++ server/db.js
@@ -5,11 +5,9 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const pool = new Pool ({    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 })
 
 export default pool;
